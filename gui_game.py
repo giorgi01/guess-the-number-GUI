0f1adb06bafd4b458d8efc3ecaa2a7d7beafb99c
Giorgi Tamarashvili <46628664+giorgi01@users.noreply.github.com>
2019-05-28 20:15:21 +0400

    Upd

--- gui_game.py
+++ gui_game.py
@@ -4,31 +4,37 @@ from random import randrange
 index = 0
 
 
-def checker(rand_num = randrange(1,26)):
-	global index
-	global num
-	index += 1 
-	if index >= 3 and index < 5:
-		if rand_num - 5 < 0:
-			print(f'რიცხვი მდებარეობს {rand_num+randrange(3,6)}-სა და 0-ს შორის')
-		else:
-			print(f'რიცხვი მდებარეობს {rand_num+randrange(3,6)}-სა და {rand_num-randrange(3,6)}-ს შორის')
-	elif index >= 5:
-		if rand_num - 3 < 0:
-			print(f'რიცხვი მდებარეობს {rand_num+randrange(2,4)}-სა და 0-ს შორის')
-		else:
-			print(f'რიცხვი მდებარეობს {rand_num+randrange(2,4)}-სა და {rand_num-randrange(2,4)}-ს შორის')
-	if int(num.get()) == rand_num:
-		print('პასუხი სწორია')
-	else:
-		print('პასუხი არასწორია, კიდევ სცადეთ :)')
+def checker(rand_num=randrange(1, 26)):
+    global index
+    global num
+    index += 1
+    if 3 <= index < 5:
+        if rand_num - 5 < 0:
+            print(f'რიცხვი მდებარეობს {rand_num+randrange(3,6)}-სა'
+                  f' და 0-ს შორის')
+        else:
+            print(f'რიცხვი მდებარეობს {rand_num+randrange(3,6)}-სა'
+                  f' და {rand_num-randrange(3,6)}-ს შორის')
+    elif index >= 5:
+        if rand_num - 3 < 0:
+            print(f'რიცხვი მდებარეობს {rand_num+randrange(2,4)}-სა'
+                  f' და 0-ს შორის')
+        else:
+            print(f'რიცხვი მდებარეობს {rand_num+randrange(2,4)}-სა'
+                  f' და {rand_num-randrange(2,4)}-ს შორის')
+    if int(num.get()) == rand_num:
+        print('პასუხი სწორია')
+    else:
+        print('პასუხი არასწორია, კიდევ სცადეთ :)')
+
 
 root = Tk()
 root.title('გამოცნობანა')
 root.geometry('300x115')
-Label(root, text='შეიყვანეთ რიცხვი 1-25 შუალედში', height=2, width=40).grid(row=0)
-num=Entry(root, width=15)
+Label(root, text='შეიყვანეთ რიცხვი 1-25 შუალედში', height=2, width=40)\
+    .grid(row=0)
+num = Entry(root, width=15)
 num.grid(row=1)
-check_button = Button(root,text='Check!', width=5, command=checker)
+check_button = Button(root, text='Check!', width=5, command=checker)
 check_button.place(x=123, y=75)
 mainloop()
